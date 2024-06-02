b81980903e04ed078e796eefe4f7d69963e71d46
sidd00@vse.cz <Craveboy1>
2024-06-02 16:12:39 +0200

    utils changed

--- utils/parse_phonenumber.py
+++ utils/parse_phonenumber.py
@@ -1,20 +1,23 @@
 import phonenumbers
 import phonenumbers.geocoder
 import pandas
-def py_parse_phonenumber(NUM: str):
+
+def py_parse_phonenumber(num):
     try:
-        num = phonenumbers.parse(NUM, 'CZ')
-        phonenumbers.is_possible_number_with_reason(num)
+        parsed_num = phonenumbers.parse(num, 'CZ')
+        phonenumbers.is_possible_number_with_reason(parsed_num)
         return {
-            'number': num.national_number,
-            'prefix': num.country_code,
-            'country_code': phonenumbers.region_code_for_number(num),
-            'valid': phonenumbers.is_valid_number(num),
-            'possible': phonenumbers.is_possible_number(num),
+            'number': parsed_num.national_number,
+            'prefix': parsed_num.country_code,
+            'country_code': phonenumbers.region_code_for_number(parsed_num),
+            'valid': phonenumbers.is_valid_number(parsed_num),
+            'possible': phonenumbers.is_possible_number(parsed_num),
             'parsed': True
         }
-    except:
-        return {'number': NUM, 'prefix': None, 'country_code': None, 'valid': False, 'possible': False, 'parsed': False}
-def udf(df: pandas.DataFrame):
-    return df[0].map(py_parse_phonenumber)
-udf._sf_vectorized_input = pandas.DataFrame
+    except Exception as e:
+        return {'number': num, 'prefix': None, 'country_code': None, 'valid': False, 'possible': False, 'parsed': False}
+
+def udf(df: pd.DataFrame, column_name: str):
+    results = df[column_name].apply(py_parse_phonenumber)
+    parsed_df = pd.DataFrame(results.tolist())
+    return parsed_df
