957f031d7f40d29e3b9b98903b6e39a53ad6d9e7
sidd00@vse.cz <Craveboy1>
2024-06-14 11:00:06 +0200

    wip notebooks were added

--- notebooks/wip/keboola_validate_data.py
+++ notebooks/wip/keboola_validate_data.py
@@ -1,33 +1,36 @@
 import pandas as pd
 import numpy as np
-import requests
-from bs4 import BeautifulSoup
 import re
-from tqdm import tqdm 
 import phonenumbers
 import phonenumbers.geocoder
 from urllib.parse import urljoin
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
-import datetime
-import os
 
-pd.options.mode.chained_assignment = None
-
-
-RUN_EVIRONMENT = "local"
+RUN_EVIRONMENT = "keboola"
 
 if RUN_EVIRONMENT == "local":
-    DB_POMOCI_PATH ="/Users/dmitriisid/Desktop/python/webscraping/data/unmatched_corrected_urls.csv"
-    #DB_POMOCI_PATH ="/Users/dmitriisid/Desktop/python/webscraping/db_pomoci.csv"
-    MAX_WORKERS = 4
+    DB_POMOCI_PATH ="db_pomoci.csv"
+    MAX_WORKERS = 2
     SAVE_FILES_PATH = "data/"
-    MAPS_SCRAPED = "/Users/dmitriisid/Desktop/python/webscraping/data/maps_results.csv"
-    #SCRAPED_DATA_PATH = '/Users/dmitriisid/Desktop/python/webscraping/data/2_06/df_scraped_2_06.csv'
-    SCRAPED_DATA_PATH = '/Users/dmitriisid/Desktop/python/webscraping/data/8_06/df_scraped_8_06.csv'
+    MAPS_SCRAPED = "../data/maps_results.csv"
+    FLAGGED_DATA_PATH = f'../data/{datetime.datetime.now().day}_{datetime.datetime.now().strftime("%m")}/db_pomoci_flagged.csv'
+    SCRAPED_DATA_PATH = f'../data/{datetime.datetime.now().day}_{datetime.datetime.now().strftime("%m")}/scraped_data.csv'
+    if not os.path.exists("../data"):
+        os.makedirs("../data")
+    if not os.path.exists(f'../data/{datetime.datetime.now().day}_{datetime.datetime.now().strftime("%m")}'):
+        os.makedirs(f'../data/{datetime.datetime.now().day}_{datetime.datetime.now().strftime("%m")}')
+elif RUN_EVIRONMENT == "keboola":
+    DB_POMOCI_PATH = "in/tables/db_pomoci.csv"
+    MAX_WORKERS = 8
+    SAVE_FILES_PATH = "out/tables/"
+    MAPS_SCRAPED = "in/tables/maps_scraped.csv"
+    SCRAPED_DATA_PATH = "in/tables/df_scraped.csv"
+    FLAGGED_DATA_PATH = "out/tables/df_flagged.csv"
 else:
     raise EnvironmentError("this environment is not supported")
-
+    
+    
 
 def has_more_than_3_consecutive_zeros(number):
     return bool(re.search(r'0{4,}', str(number)))
@@ -53,12 +56,12 @@ def udf(df: pd.DataFrame, column_name: str):
     parsed_df = pd.DataFrame(results.tolist())
     return parsed_df
 
-def explode_df(df:pd.DataFrame, column_name:str, web_column = "Base Website", scraped_web_column = "Scraped Page") -> pd.DataFrame:
+def explode_df(df:pd.DataFrame, column_name:str, web_column = "Base_Website", scraped_web_column = "Scraped_Page") -> pd.DataFrame:
     #df_res = pd.read_csv("../data/1_06/df_scraped_full.csv")
     df_res = df
     df_res[f'{column_name}_scraped'] = df_res[column_name].str.split(', ')
    # df_res['emails_scraped'] = df_res['Emails'].str.split(', ')
-    if scraped_web_column != "Scraped Page":
+    if scraped_web_column != "Scraped_Page":
         df_res_phones = df_res[[web_column,f"{column_name}_scraped"]]
         df_res_exp = df_res_phones.explode(f'{column_name}_scraped').reset_index(drop=True)
         return df_res_exp
@@ -67,7 +70,7 @@ def explode_df(df:pd.DataFrame, column_name:str, web_column = "Base Website", sc
     return df_res_exp
 
 
-def clean_scraped_phones(df: pd.DataFrame, phone_scraped_column = "Phone Numbers", web_column = "Base Website",scraped_web_column = "Scraped Page") -> pd.DataFrame:
+def clean_scraped_phones(df: pd.DataFrame, phone_scraped_column = "Phone_Numbers", web_column = "Base_Website",scraped_web_column = "Scraped_Page") -> pd.DataFrame:
     phones_exp = explode_df(df,phone_scraped_column,web_column,scraped_web_column)
     ress_df = udf(phones_exp,f"{phone_scraped_column}_scraped")
     phones_exp["formated_number"] = ress_df["formated_number"]
@@ -96,7 +99,7 @@ def clean_email(email):
         cleaned_emails.append(email)
     return ', '.join(cleaned_emails)
 
-def clean_scraped_emails(df: pd.DataFrame, email_scraped_column = "Emails", web_column = "Base Website") -> pd.DataFrame:
+def clean_scraped_emails(df: pd.DataFrame, email_scraped_column = "Emails", web_column = "Base_Website") -> pd.DataFrame:
     emails_exp = explode_df(df,email_scraped_column)
     emails_exp[f'{email_scraped_column}_scraped'] = emails_exp[f'{email_scraped_column}_scraped'].apply(clean_email)
     emails_deduped = emails_exp.drop_duplicates(subset=[web_column, f'{email_scraped_column}_scraped'])
@@ -107,20 +110,18 @@ def db_pomoci_transform(df:pd.DataFrame) -> pd.DataFrame:
     """
     This function prepare data initial data for validation
     """
-    df['E-mail'] = df['E-mail'].apply(clean_email)
+    df['E_mail'] = df['E_mail'].apply(clean_email)
     ress_df = udf(df,"Telefon")
-    print(ress_df.values)
     df['Telefon'] = ress_df["formated_number"]
-    df = df[~df["Webová stránka"].isna()]
-    df.loc[df['Webová stránka'].str.startswith('www'), 'web'] = df['Webová stránka'].str.replace('^www', 'https://www', regex=True)
+    df = df[~df["Webova_stranka"].isna()]
+    df.loc[df['Webova_stranka'].str.startswith('www'), 'web'] = df['Webova_stranka'].str.replace('^www', 'https://www', regex=True)
     return df
-
-    
+  
 def main():
     start_time = time.time()
-    db_pomoci = pd.read_csv(DB_POMOCI_PATH,dtype={'Telefon': str})
-    result_scraper = pd.read_csv(SCRAPED_DATA_PATH,dtype={'Phone': str})
-    maps_results = pd.read_csv(MAPS_SCRAPED, sep=";")
+    db_pomoci = pd.read_csv(DB_POMOCI_PATH)
+    result_scraper = pd.read_csv(SCRAPED_DATA_PATH)
+    maps_results = pd.read_csv(MAPS_SCRAPED, sep=",")
 
 
     print("Data were loaded ")
@@ -134,33 +135,25 @@ def main():
 
     df_phones_scraped = clean_scraped_phones(result_scraper)
     df_emails_scraped = clean_scraped_emails(result_scraper)
-    df_emails_scraped = df_emails_scraped[['Base Website', 'Scraped Page', 'Emails_scraped']]
+    df_emails_scraped = df_emails_scraped[['Base_Website', 'Scraped_Page', 'Emails_scraped']]
     df_emails_scraped['Contact_type'] = 'Email'
     df_emails_scraped.rename(columns={'Emails_scraped': 'Contact'}, inplace=True)
 
-    df_phones_scraped = df_phones_scraped[['Base Website', 'Scraped Page', 'formated_number']]
+    df_phones_scraped = df_phones_scraped[['Base_Website', 'Scraped_Page', 'formated_number']]
     df_phones_scraped['Contact_type'] = 'Phone'
     df_phones_scraped.rename(columns={'formated_number': 'Contact'}, inplace=True)
 
     # Combine the DataFrames by appending rows
     combined_df = pd.concat([df_phones_scraped, df_emails_scraped], ignore_index=True)
-    combined_df.to_csv("contact_df.csv")
 
     print("Scraped data are ready ")
     print("")
 
     db_pomoci = db_pomoci_transform(db_pomoci)
-    #TODO: TMP PART
-    # db_pomoci = db_pomoci[db_pomoci["Webová stránka"].isin(pd.read_csv("/Users/dmitriisid/Desktop/python/webscraping/data/unmatched_corrected_urls.csv")["Webová stránka"])]
-    # merged_df = db_pomoci.merge(pd.read_csv("/Users/dmitriisid/Desktop/python/webscraping/data/unmatched_corrected_urls.csv"), on='Webová stránka', how='left')
-
-    # # Replacing "Webová stránka" values in df1 with "Valid url" values from df2
-    # db_pomoci['Webová stránka'] = merged_df['Valid Web']
-    #########
     print("DB data are ready ")
     print("")
-
-    maps_results = clean_scraped_phones(maps_results,"API Phone", web_column = "Web", scraped_web_column =  "Web")
+    print(maps_results.head())
+    maps_results = clean_scraped_phones(maps_results,phone_scraped_column = "API_Phone", web_column = "Web", scraped_web_column =  "Web")
     #maps_results = clean_scraped_emails(maps_results, "Email")
 
     maps_contacts = pd.concat([
@@ -193,7 +186,7 @@ def main():
             sources.add('maps_contacts_telefon')
 
         # Check against scraped contacts
-        if check_contact(row['E-mail'], scraped_contacts, 'scraped_contacts'):
+        if check_contact(row['E_mail'], scraped_contacts, 'scraped_contacts'):
             sources.add('scraped_contacts_email')
         if check_contact(row['Telefon'], scraped_contacts, 'scraped_contacts'):
             sources.add('scraped_contacts_telefon')
@@ -210,42 +203,16 @@ def main():
     print(f"Data baze obsahuje {matched_num} schodnych kontaktu a {unmatched_num} neschodnych kontaktu")
     print("")
 
-    # def find_new_contact(row):
-    #     if row['Matched'] == 'unmatched':
-    #         web = row['Webová stránka']
-    #         maps_contact = maps_contacts[maps_contacts['Web'].str.contains(web, na=False) & ~(maps_contacts["Contact"].isna())]
-    #         scraped_contact = combined_df[combined_df['Base Website'].str.contains(web, na=False) & ~(combined_df["Contact"].isna())]
-            
-    #         # Find common contacts in both maps_contact and scraped_contact
-    #         common_contacts = pd.merge(maps_contact, scraped_contact, on='Contact')
-    #         scraped_email_contact = combined_df[(combined_df['Base Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Email") & ~(combined_df["Contact"].isna())]
-    #         scraped_phone_contact = combined_df[(combined_df['Base Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Phone") & ~(combined_df["Contact"].isna())]
-    #         new_contact = []
-    #         if not common_contacts.empty and not scraped_email_contact.empty:
-    #             new_contact.extend(common_contacts['Contact'].values)
-    #             new_contact.extend(scraped_email_contact['Contact'].values)
-    #             return pd.Series([new_contact, 'new_contact_both_match_with_email'])
-    #         if not common_contacts.empty  and  scraped_email_contact.empty:
-    #             new_contact.extend(common_contacts['Contact'].values)
-    #             return pd.Series([new_contact, 'new_contact_both_match'])
-    #         if  common_contacts.empty  and  not scraped_email_contact.empty:
-    #             new_contact.extend(scraped_email_contact['Contact'].values)
-    #             return pd.Series([new_contact, 'new_email_match'])
-    #         if  common_contacts.empty  and  not scraped_phone_contact.empty:
-    #             new_contact.extend(scraped_phone_contact['Contact'].values)
-    #             return pd.Series([new_contact, 'new_phone_match'])
-
-    #     return pd.Series([None, None])
     def find_new_contact(row):
         if row['Matched'] == 'unmatched':
-            web = str(row['Webová stránka'])
+            web = row['Webova_stranka']
             maps_contact = maps_contacts[maps_contacts['Web'].str.contains(web, na=False) & ~(maps_contacts["Contact"].isna())]
-            scraped_contact = combined_df[combined_df['Base Website'].str.contains(web, na=False) & ~(combined_df["Contact"].isna())]
+            scraped_contact = combined_df[combined_df['Base_Website'].str.contains(web, na=False) & ~(combined_df["Contact"].isna())]
             
             # Find common contacts in both maps_contact and scraped_contact
             common_contacts = pd.merge(maps_contact, scraped_contact, on='Contact')
-            scraped_email_contact = combined_df[(combined_df['Base Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Email") & ~(combined_df["Contact"].isna())]
-            scraped_phone_contact = combined_df[(combined_df['Base Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Phone") & ~(combined_df["Contact"].isna())]
+            scraped_email_contact = combined_df[(combined_df['Base_Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Email") & ~(combined_df["Contact"].isna())]
+            scraped_phone_contact = combined_df[(combined_df['Base_Website'].str.contains(web, na=False)) & (combined_df["Contact_type"] == "Phone") & ~(combined_df["Contact"].isna())]
             new_contact = []
             if not common_contacts.empty and not scraped_email_contact.empty:
                 new_contact.extend(common_contacts['Contact'].values)
@@ -262,7 +229,6 @@ def main():
                 return pd.Series([new_contact, 'new_phone_match'])
 
         return pd.Series([None, None])
-
     find_new_contact_start = time.time()
     
     db_pomoci[['New Contact', 'New Matched']] = db_pomoci.apply(find_new_contact, axis=1)
@@ -274,18 +240,18 @@ def main():
     db_pomoci['Matched'] = db_pomoci.apply(
         lambda row: row['New Matched'] if pd.notna(row['New Matched']) else row['Matched'], axis=1)
     
-    db_pomoci['E-mail'] = db_pomoci.apply(
-        lambda row: next((contact for contact in row['New Contact'] if pd.isna(row['E-mail']) and pd.notna(contact)), row['E-mail'])
-        if row['New Contact'] is not None else row['E-mail'], axis=1)
+    db_pomoci['E_mail'] = db_pomoci.apply(
+        lambda row: next((contact for contact in row['New Contact'] if pd.isna(row['E_mail']) and pd.notna(contact)), row['E_mail'])
+        if row['New Contact'] is not None else row['E_mail'], axis=1)
 
     db_pomoci['Telefon'] = db_pomoci.apply(
         lambda row: next((contact for contact in row['New Contact'] if pd.isna(row['Telefon']) and pd.notna(contact)), row['Telefon'])
         if row['New Contact'] is not None else row['Telefon'], axis=1)
 
     # Drop the helper columns
-    #db_pomoci.drop(columns=['New Contact', 'New Matched'], inplace=True)
-
-    db_pomoci.to_csv('db_pomoci_flagged_with_new_contacts_V3.csv', index=False)
+    
+    db_pomoci.drop(columns=["web"], inplace= True)
+    db_pomoci.to_csv('out/tables/db_pomoci_flagged.csv', index=False)
 
     matched_num = db_pomoci[db_pomoci["Matched"]=="matched"].shape[0]
     unmatched_num = db_pomoci[db_pomoci["Matched"]=="unmatched"].shape[0]
@@ -297,4 +263,6 @@ def main():
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Time taken: {elapsed_time:.2f} seconds")
+
 main()
+
